ad597a20576c94e7ecbb0832ac6cb25e297ab32b
Amir Pasha Bagheri <a.p.bagheri83@gmail.com>
2021-02-27 15:17:09 +0330

    Updating Server Side and Front end

--- public/client.js
+++ public/client.js
@@ -2,18 +2,33 @@ const socket = io()
 const sendMessage = document.getElementById('submit')
 const scrollDown = document.getElementById('message-container')
 
+//Getting Name From Query
+const username = Qs.parse(location.search,{
+    ignoreQueryPrefix : true
+})
+
 function createBotMessage (data){
     const div = document.createElement('div')
     div.innerHTML = `<p class="my-2 rounded w-50 mx-auto" style="text-align: center; background-color: rgb(207, 196, 196);">${data}</p>`
     document.getElementById('message-container').appendChild(div)
 }
 
-function createMessage (obj){
+function createUsersMessage (obj){
     const div = document.createElement('div')
     div.innerHTML = 
     `<p class="my-2 mx-4 px-3 py-1 rounded-right bg-white border border-secondary" style="word-wrap: break-word;">
-    <b class="alert-primary rounded px-1">Sofie <small><i>15:05</i></small></b>
-    <br/>${obj}</p>`
+    <b class="alert-primary rounded px-1">${obj.username} <small><i>${obj.time}</i></small></b>
+    <br/>${obj.text}</p>`
+    document.getElementById('message-container').appendChild(div)
+    console.log(obj);
+}
+
+function createYourMessage (obj){
+    const div = document.createElement('div')
+    div.innerHTML = 
+    `<p class="my-2 mx-4 px-3 py-1 rounded-right alert-success border border-secondary" style="word-wrap: break-word;">
+    <b class="bg-success text-white rounded px-1">You <small><i>${obj.time}</i></small></b>
+    <br/>${obj.text}</p>`
     document.getElementById('message-container').appendChild(div)
 }
 
@@ -29,14 +44,38 @@ sendMessage.addEventListener('click',(e)=>{
     e.preventDefault()
     const message = document.getElementById('Message').value
     if(message!==''){
-    socket.emit('Message',message)
+    socket.emit('Message',username , message)
     document.getElementById('Message').value = ''
     }
     else false
 })
 
+//User Join or Left
+socket.emit('Bot',username)
+
 //Showing Message From Server
-socket.on('User-Message',data=>{
-    createMessage(data)
+socket.on('Users-Message',data=>{
+    createUsersMessage(data)
     scrollDown.scrollTop = scrollDown.scrollHeight
+})
+
+socket.on('Your-Message',data=>{
+    createYourMessage(data)
+    scrollDown.scrollTop = scrollDown.scrollHeight
+})
+
+//Add To Members
+const memberList = document.getElementById('member-list')
+
+socket.emit('Add-Member',username)
+socket.on('Pass-Array-Add',membersdata=>{
+    const mapMembers = membersdata.map(user=>`<li>${user}</li>`)
+    memberList.innerHTML = mapMembers.join('<hr/>')
+})
+
+//Remove From Members
+socket.emit('Remove-Member',username)
+socket.on('Pass-Array-Remove',membersdata=>{
+    const mapMembers = membersdata.map(user=>`<li>${user}</li>`)
+    memberList.innerHTML = mapMembers.join('<hr/>')
 })
--- server.js
+++ server.js
@@ -2,10 +2,23 @@ const express = require('express')
 const http = require('http')
 const path = require('path')
 const socketio = require('socket.io')
+const moment = require('moment')
 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
+
+const members = []
+
+//User Message 
+const Message = (username , text) =>{
+  return {
+    username,
+    text,
+    time: moment().format('h:mm a'),
+    id: members.length
+  }
+} 
 
 //message if You Enter the chat
 const manageMessagea = {
@@ -24,9 +37,31 @@ app.use(express.static(path.join(__dirname,'public')))
 
 io.on('connection', socket =>{
   manageMessagea.Ujoin(socket,'You Joined The Chat !')
-  manageMessagea.Userjoin(socket,`Someone Joined The Chat !`)
-  socket.on('disconnect',()=>manageMessagea.Userleft(socket,`Someone Left The Chat !`))
-  socket.on('Message',(message)=> io.emit('User-Message',message))
+
+  //Adding To Memberlist
+  socket.on('Add-Member',username=>{
+    members.push(username.username)
+    io.emit('Pass-Array-Add',members)
+  })
+
+  //Removing From Memberlist
+  socket.on('Remove-Member',username=>{
+    socket.on('disconnect',()=>{
+      const index = members.indexOf(username.username)
+      const newMembers = members.splice(index,1)
+      io.emit('Pass-Array-Remove',members)
+    })
+  })
+  
+  socket.on('Bot',username=>{
+    manageMessagea.Userjoin(socket,`${username.username} Joined The Chat !`) 
+    socket.on('disconnect',()=>manageMessagea.Userleft(socket,`${username.username} Left The Chat !`))
+  })
+
+  socket.on('Message',(username,message)=> {
+    socket.broadcast.emit('Users-Message',Message(username.username,message))
+    socket.emit('Your-Message',Message('You',message))
+  })
 })
 
 server.listen(3000,()=>console.log('Running On Port 3000...'))
